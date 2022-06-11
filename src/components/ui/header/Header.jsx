a891ebf0d6c8c5e35580939b1c6e98b449a8b3cd
tatadavila <tatdev91@gmail.com>
2022-06-11 11:21:27 -0500

    erease hello world

--- src/components/ui/header/Header.jsx
+++ src/components/ui/header/Header.jsx
@@ -9,7 +9,6 @@ export const Header = () => {
   return (
     <section className="header">
       <img src="https://firebasestorage.googleapis.com/v0/b/landing-page-52729.appspot.com/o/assets%2Flogo_pioneras.png?alt=media&token=f683d663-ddc4-4a39-84d6-2aab73c8ddd2" />
-      Hello World
     </section>
   );
 };
