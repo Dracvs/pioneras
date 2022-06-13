9d2d4a41284a7e49fcc0ae0d992185fa6d1adecb
tatadavila <tatdev91@gmail.com>
2022-06-13 00:41:38 -0500

    Finish edition of header component with styles

--- src/components/ui/header/Header.jsx
+++ src/components/ui/header/Header.jsx
@@ -1,14 +1,18 @@
-// @vendors
-import { useEffect } from "react";
-
-// @firebase
-
 export const Header = () => {
-  //const storageRef = app.storage().ref()
-
-  return (
-    <section className="header">
-      <img src="https://firebasestorage.googleapis.com/v0/b/landing-page-52729.appspot.com/o/assets%2Flogo_pioneras.png?alt=media&token=f683d663-ddc4-4a39-84d6-2aab73c8ddd2" />
-    </section>
-  );
-};
+    return (
+        <header className="wave-container ">
+            <img
+                className="logo"
+                src="https://firebasestorage.googleapis.com/v0/b/landing-page-52729.appspot.com/o/assets%2Flogo_pioneras.png?alt=media&token=f683d663-ddc4-4a39-84d6-2aab73c8ddd2"
+            />
+            <h3 className="header-text">
+                Hola :) Estamos trabajando en nuestra página web, y muy pronto
+                podrás descubrirla 🙌
+            </h3>
+            <h3 className="header-text">
+                Por el momento, aquí podrás conocer un poquito más de nuestra
+                comunidad 👇
+            </h3>
+        </header>
+    )
+}
